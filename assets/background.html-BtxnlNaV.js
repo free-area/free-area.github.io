3ec21b80418a9ccfef3ae820d940b790e888d796
free-area <81863791+free-area@users.noreply.github.com>
2023-12-03 05:39:44 +0000

    Deploying to gh-pages from @ free-area/free-area.github.io@3ca412c292e3b5813e5efae59a43a75d0a6be233 🚀

---
+++ assets/background.html-BtxnlNaV.js
@@ -0,0 +1,65 @@
+import{_ as v}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as d,o as u,c as h,d as l,w as a,a as e,b as n,f as r}from"./app-1w4_OrWF.js";const o={},m=r(`<h1 id="介绍" tabindex="-1"><a class="header-anchor" href="#介绍" aria-hidden="true">#</a> 介绍</h1><h2 id="容器" tabindex="-1"><a class="header-anchor" href="#容器" aria-hidden="true">#</a> 容器</h2><h3 id="信息容器" tabindex="-1"><a class="header-anchor" href="#信息容器" aria-hidden="true">#</a> 信息容器</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>::: info 自定义标题
+
+信息容器，包含 \`代码\` 与 [链接](#提示容器)。
+
+\`\`\`js
+const a = 1;
+\`\`\`
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="hint-container info"><p class="hint-container-title">自定义标题</p><p>信息容器，包含 <code>代码</code> 与 <a href="#%E6%8F%90%E7%A4%BA%E5%AE%B9%E5%99%A8">链接</a>。</p><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></div><h3 id="提示容器" tabindex="-1"><a class="header-anchor" href="#提示容器" aria-hidden="true">#</a> 提示容器</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>::: tip 自定义标题
+
+提示容器
+
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="hint-container tip"><p class="hint-container-title">自定义标题</p><p>提示容器</p></div><h3 id="警告容器" tabindex="-1"><a class="header-anchor" href="#警告容器" aria-hidden="true">#</a> 警告容器</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>::: warning 自定义标题
+
+警告容器
+
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="hint-container warning"><p class="hint-container-title">自定义标题</p><p>警告容器</p></div><h3 id="危险容器" tabindex="-1"><a class="header-anchor" href="#危险容器" aria-hidden="true">#</a> 危险容器</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>::: caution 自定义标题
+
+危险容器
+
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="hint-container caution"><p class="hint-container-title">自定义标题</p><p>危险容器</p></div><h3 id="详情容器" tabindex="-1"><a class="header-anchor" href="#详情容器" aria-hidden="true">#</a> 详情容器</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>::: details 自定义标题
+
+详情容器
+
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><details class="hint-container details"><summary>自定义标题</summary><p>详情容器</p></details><h2 id="选项卡" tabindex="-1"><a class="header-anchor" href="#选项卡" aria-hidden="true">#</a> 选项卡</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>::: code-tabs
+
+@tab pnpm
+
+\`\`\`bash
+pnpm add -D vuepress-theme-hope
+\`\`\`
+
+@tab yarn
+
+\`\`\`bash
+yarn add -D vuepress-theme-hope
+\`\`\`
+
+@tab:active npm
+
+\`\`\`bash
+npm i -D vuepress-theme-hope
+\`\`\`
+
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,19),p=e("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[e("pre",{class:"language-bash"},[e("code",null,[e("span",{class:"token function"},"pnpm"),n(),e("span",{class:"token function"},"add"),n(),e("span",{class:"token parameter variable"},"-D"),n(` vuepress-theme-hope
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"})])],-1),b=e("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[e("pre",{class:"language-bash"},[e("code",null,[e("span",{class:"token function"},"yarn"),n(),e("span",{class:"token function"},"add"),n(),e("span",{class:"token parameter variable"},"-D"),n(` vuepress-theme-hope
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"})])],-1),g=e("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[e("pre",{class:"language-bash"},[e("code",null,[e("span",{class:"token function"},"npm"),n(" i "),e("span",{class:"token parameter variable"},"-D"),n(` vuepress-theme-hope
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"})])],-1),x=r(`<h2 id="对齐" tabindex="-1"><a class="header-anchor" href="#对齐" aria-hidden="true">#</a> 对齐</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>::: center
+
+我是居中的
+
+:::
+
+::: right
+
+我在右对齐
+
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div style="text-align:center;"><p>我是居中的</p></div><div style="text-align:right;"><p>我在右对齐</p></div><h2 id="属性支持" tabindex="-1"><a class="header-anchor" href="#属性支持" aria-hidden="true">#</a> 属性支持</h2><h3 id="id" tabindex="-1"><a class="header-anchor" href="#id" aria-hidden="true">#</a> id</h3><p>一个拥有 ID 的 <strong id="word">单词</strong>。</p><p>\\</p><p>\\</p><p>\\</p><p>\\</p><p><a href="#word">跳转单词</a></p><h2 id="脚注" tabindex="-1"><a class="header-anchor" href="#脚注" aria-hidden="true">#</a> 脚注</h2><p>此文字有脚注<a href="%E8%BF%99%E6%98%AF%E8%84%9A%E6%B3%A8%E5%86%85%E5%AE%B9">^first</a>.</p><p>\\</p><p>\\</p><p>\\</p><h2 id="任务列表" tabindex="-1"><a class="header-anchor" href="#任务列表" aria-hidden="true">#</a> 任务列表</h2><ul><li>[ ] 计划1</li><li>[x] 计划二</li></ul>`,19),f={href:"https://theme-hope.vuejs.press/zh/guide/markdown/tasklist.html",target:"_blank",rel:"noopener noreferrer"};function _(k,E){const t=d("CodeTabs"),c=d("ExternalLinkIcon");return u(),h("div",null,[m,l(t,{id:"56",data:[{id:"pnpm"},{id:"yarn"},{id:"npm"}],active:2},{title0:a(({value:i,isActive:s})=>[n("pnpm")]),title1:a(({value:i,isActive:s})=>[n("yarn")]),title2:a(({value:i,isActive:s})=>[n("npm")]),tab0:a(({value:i,isActive:s})=>[p]),tab1:a(({value:i,isActive:s})=>[b]),tab2:a(({value:i,isActive:s})=>[g]),_:1}),x,e("p",null,[n("- "),e("a",f,[n("查看详情"),l(c)])])])}const w=v(o,[["render",_],["__file","background.html.vue"]]);export{w as default};
